16354c28b9dc69a8230396b81e556f8e603cc77c
janainarque <janainarque@gmail.com>
2023-08-15 21:50:17 -0300

    projeto java

---
+++ projetoBCInvesting/src/main/java/br/com/bcinvesting/ProjetoBCInvestingApplication.java
@@ -0,0 +1,13 @@
+package br.com.bcinvesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBCInvestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBCInvestingApplication.class, args);
+	}
+
+}

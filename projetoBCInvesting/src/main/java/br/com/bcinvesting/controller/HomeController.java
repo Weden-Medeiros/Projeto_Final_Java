cec3f24b001448013bc38ed8fdae962af830026d
Janaina Arque <janainarque@gmail.com>
2023-08-16 21:55:42 -0300

    trabalhando tela de cadastro de usuario e login

--- projetoBCInvesting/src/main/java/br/com/bcinvesting/controller/HomeController.java
+++ projetoBCInvesting/src/main/java/br/com/bcinvesting/controller/HomeController.java
@@ -1,12 +1,25 @@
 package br.com.bcinvesting.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController{
+	
+	private AdministradoresRepo repo;
+	
 	@GetMapping("/")
 	public String index(){
 		return "home/index";
 	}
+	
+	
+	@PostMapping("/logar")
+	public String logar(Model model, Administrador admParam, String senha, String lembrar){
+		return "login/index";
+	}
+	
+	
 }
